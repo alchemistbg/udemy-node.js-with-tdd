7da146b2b361d64a50903e771f19f1261aa230cc
alchemistbg <alchemist_bg@abv.bg>
2022-01-19 20:19:30 +0100

    Minor fixes in the configurations of the project

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const app = require('./src/app');
+const sequelize = require('./src/config/database');
+sequelize.sync();
 
 app.listen(3000, () => {
     console.log('Server is up and running on port 3000');
--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize('hoaxify', 'dbUser', 'dbPass', {
 	dialect: 'sqlite',
 	storage: './database.sqlite',
+	logging: false,
 });
 
 module.exports = sequelize;
