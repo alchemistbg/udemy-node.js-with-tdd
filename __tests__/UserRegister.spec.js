f129a007c77ae9b97f2ba88f2a049822fec36f0f
alchemistbg <alchemist_bg@abv.bg>
2022-01-19 20:16:57 +0100

    Refactor tests
    All test are now using async/await feature in JS

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -25,62 +25,34 @@ describe('Test user registration functionality', () => {
 		});
 	};
 
-	// it('return 200 when signup request is valid', (done) => {
-	// 	postValidUser().expect(200, done);
-	// });
-
-	//This is another variant of this test that uses then clause
-	it('return 200 when signup request is valid', (done) => {
-		postValidUser().then((response) => {
-			expect(response.status).toBe(200);
-			done();
-		});
+	it('return 200 when signup request is valid', async () => {
+		const response = await postValidUser();
+		expect(response.status).toBe(200);
 	});
 
-	// This is another variant of this test that uses async/await.
-	// There is fake parsing error from eslint: doesn't like the fat arrow =>
-	// it('return 200 when signup request is valid', async () => {
-	// 	const response = await postValidUser();
-	// 	expect(response.status).toBe(200);
-	// });
-
-	it('return success message when signup request is valid', (done) => {
-		postValidUser().then((response) => {
-			expect(response.body.message).toBe('User created');
-			done();
-		});
+	it('return success message when signup request is valid', async () => {
+		const response = await postValidUser();
+		expect(response.body.message).toBe('User created');
 	});
 
-	it('saves user to the database', (done) => {
-		postValidUser().then(() => {
-			// Query the users table
-			User.findAll().then((userList) => {
-				expect(userList.length).toBe(1);
-				done();
-			});
-		});
+	it('saves user to the database', async () => {
+		await postValidUser();
+		const userList = await User.findAll();
+		expect(userList.length).toBe(1);
 	});
 
-	it('saves username and email to the database', (done) => {
-		postValidUser().then(() => {
-			// Query the users table
-			User.findAll().then((userList) => {
-				const savedUser = userList[0];
-				expect(savedUser.username).toBe('user1');
-				expect(savedUser.email).toBe('user1@mail.com');
-				done();
-			});
-		});
+	it('saves username and email to the database', async () => {
+		await postValidUser();
+		const userList = await User.findAll()
+		const savedUser = userList[0];
+		expect(savedUser.username).toBe('user1');
+		expect(savedUser.email).toBe('user1@mail.com');
 	});
 
-	it('hashes password when saving user to the database', (done) => {
-		postValidUser().then(() => {
-			// Query the users table
-			User.findAll().then((userList) => {
-				const savedUser = userList[0];
-				expect(savedUser.password).not.toBe('P4ssword');
-				done();
-			});
-		});
+	it('hashes password when saving user to the database', async () => {
+		await postValidUser();
+		const userList = await User.findAll();
+		const savedUser = userList[0];
+		expect(savedUser.password).not.toBe('P4ssword');
 	});
 });
