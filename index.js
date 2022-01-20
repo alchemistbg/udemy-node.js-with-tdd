1900e7366192bfb81a51adad908e72d84b6e679e
alchemistbg <alchemist_bg@abv.bg>
2022-01-20 12:43:53 +0100

    Minor code changes

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const sequelize = require('./src/config/database');
 sequelize.sync();
 
 app.listen(3000, () => {
-    console.log('Server is up and running on port 3000');
+	console.log('Server is up and running on port 3000');
 });
