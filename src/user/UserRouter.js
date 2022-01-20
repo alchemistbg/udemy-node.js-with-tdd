0638fde74ee4d79c95afc8028666ee22a59687d2
alchemistbg <alchemist_bg@abv.bg>
2022-01-20 16:23:17 +0100

    Replace the separate tests for each field with each() function in jest

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -1,37 +1,26 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 
 const UserService = require('./UserService');
 
 const router = express.Router();
 
-const validateUsername = (req, res, next) => {
-	const user = req.body;
-	if (user.username === null) {
-		req.validationErrors = {
-			username: 'Username cannot be null',
-		};
-	}
-	next();
-};
-
-const validateEmail = (req, res, next) => {
-	const user = req.body;
-	if (user.email === null) {
-		req.validationErrors = {
-			...req.validationErrors,
-			email: 'Email cannot be null',
+router.post(
+	'/',
+	check('username').notEmpty().withMessage('Username cannot be null!'),
+	check('email').notEmpty().withMessage('Email cannot be null!'),
+	check('password').notEmpty().withMessage('Password cannot be null!'),
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			const validationErrors = {};
+			errors.array().forEach((error) => {
+				return (validationErrors[error.param] = error.msg);
+			});
+			return res.status(400).send({ validationErrors: validationErrors });
 		}
-	}
-	next();
-};
-
-router.post('/', validateUsername, validateEmail, async (req, res) => {
-	if (req.validationErrors) {
-		const response = { validationErrors: { ...req.validationErrors } };
-		return res.status(400).send(response);
-	}
-	await UserService.save(req.body);
-	return res.status(200).send({ message: 'User created' });
+		await UserService.save(req.body);
+		return res.status(200).send({ message: 'User created' });
 });
 
 module.exports = router;
