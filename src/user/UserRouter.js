1053910e96802387bc67ba028e8984b8cdf9478c
alchemistbg <alchemist_bg@abv.bg>
2022-01-20 12:41:56 +0100

    Add check for empty username

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -5,8 +5,16 @@ const UserService = require('./UserService');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-	await UserService.save(req.body);
+	const user = req.body;
+	if (user.username === null) {
+		return res.status(400).send({
+			validationErrors: {
+				username: 'Username cannot be null',
+			},
+		});
+	}
 
+	await UserService.save(req.body);
 	return res.status(200).send({ message: 'User created' });
 });
 
