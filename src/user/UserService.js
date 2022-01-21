6d1bc2293ad28d3cb90f4e542a8fede90d4edb9d
alchemistbg <alchemist_bg@abv.bg>
2022-01-21 14:12:47 +0100

    Add findByEmail function

--- src/user/UserService.js
+++ src/user/UserService.js
@@ -22,6 +22,11 @@ const save = async (body) => {
 	await User.create(user);	
 }
 
+const findByEmail = async (email) => {
+	return await User.findOne({ where: { email: email } });
+};
+
 module.exports = {
 	save,
+	findByEmail,
 };
