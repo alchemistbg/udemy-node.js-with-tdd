9877b07be3a7f576f40e807d0ba798a697659322
alchemistbg <alchemist_bg@abv.bg>
2022-01-21 14:37:24 +0100

    Add force:true option to sequelize.sync

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require('./src/app');
 const sequelize = require('./src/config/database');
-sequelize.sync();
+
+sequelize.sync({ force: true });
 
 app.listen(3000, () => {
 	console.log('Server is up and running on port 3000');
