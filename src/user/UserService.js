dbf578aa829adda5041b1a756a5c6840898063d9
alchemistbg <alchemist_bg@abv.bg>
2022-01-22 12:50:28 +0100

    Add function that generates tokens needed for activation

--- src/user/UserService.js
+++ src/user/UserService.js
@@ -1,26 +1,33 @@
 const bcrypt = require('bcrypt');
+const crypto = require('crypto');
 
 const User = require('./User');
 
+const generateToken = (length) => {
+	// return crypto.randomBytes(length).toString('hex');
+	return crypto.randomBytes(length).toString('hex').substring(0, length);
+}
+
 const save = async (body) => {
-	const hash = await bcrypt.hash(body.password, 10);
+	const { username, email, password } = body;
+	const hash = await bcrypt.hash(password, 10);
 	// First way to create user object
-	const user = { ...body, password: hash };
-	
+	const user = { username, email, password: hash, activationToken: generateToken(16) };
+
 	// Second way to create user object
 	// const user = {
 	// 	username: req.body.username,
 	// 	email: req.username.email,
 	// 	password: hash,
 	// }
-	
+
 	// Third way to create user object
 	// const user = Object.assign({}, req.body, {
 	// 	password: hash,
 	// });
-	
-	await User.create(user);	
-}
+
+	await User.create(user);
+};
 
 const findByEmail = async (email) => {
 	return await User.findOne({ where: { email: email } });
