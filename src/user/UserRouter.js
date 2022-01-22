0a6fd014f197b23ce872da06b9bcf3433ea29b57
alchemistbg <alchemist_bg@abv.bg>
2022-01-22 12:42:53 +0100

    Refactor UserRouter to work with different locales

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -6,45 +6,41 @@ const router = express.Router();
 
 router.post(
 	'/',
-	check('username')
-		.notEmpty()
-		.withMessage('Username cannot be null!')
-		.bail()
-		.isLength({ min: 4, max: 32 })
-		.withMessage('Must have min 4 and max 32 characters!'),
+	check('username').notEmpty().withMessage('username_null').bail().isLength({ min: 4, max: 32 }).withMessage('username_size'),
 	check('email')
 		.notEmpty()
-		.withMessage('Email cannot be null!')
+		.withMessage('email_null')
 		.bail()
 		.isEmail()
-		.withMessage('Email must be a valid email!')
+		.withMessage('email_invalid')
 		.bail()
 		.custom(async (email) => {
 			const user = await UserService.findByEmail(email);
 			if (user) {
-				throw new Error('Email already in use!');
+				throw new Error('email_inuse');
 			}
 		}),
 	check('password')
 		.notEmpty()
-		.withMessage('Password cannot be null!')
+		.withMessage('password_null')
 		.bail()
 		.isLength({ min: 8, max: 16 })
-		.withMessage('Must have min 8 and max 16 characters!')
+		.withMessage('password_size')
 		.bail()
 		.matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$/)
-		.withMessage('Password must have at least 1 uppercase, 1 lowercase letter as well as 1 number!'),
+		.withMessage('password_pattern'),
 	async (req, res) => {
+		// console.log(req.headers['accept-language']);
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
 			const validationErrors = {};
 			errors.array().forEach((error) => {
-				return (validationErrors[error.param] = error.msg);
+				return (validationErrors[error.param] = req.t(error.msg));
 			});
 			return res.status(400).send({ validationErrors: validationErrors });
 		}
 		await UserService.save(req.body);
-		return res.status(200).send({ message: 'User created' });
+		return res.status(200).send({ message: req.t('user_created') });
 	}
 );
 
