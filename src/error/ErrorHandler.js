e676db423fd4b9265e904812d4e786e004ac73eb
alchemistbg <alchemist_bg@abv.bg>
2022-01-25 18:21:42 +0100

    Implement error handling

--- src/error/ErrorHandler.js
+++ src/error/ErrorHandler.js
@@ -1,1 +1,19 @@
+// eslint-disable-next-line no-unused-vars
+const ErrorHandler = (error, req, res, next) => {
+	const { status, message, errors } = error;
+	let validationErrors;
+	if (errors) {
+		validationErrors = {};
+		errors.forEach((error) => {
+			return (validationErrors[error.param] = req.t(error.msg));
+		});
+	}
+	res.status(status).send({
+		path: req.originalUrl,
+		timestamp: new Date().getTime(),
+		message: req.t(message),
+		validationErrors,
+	});
+};
 
+module.exports = ErrorHandler;
