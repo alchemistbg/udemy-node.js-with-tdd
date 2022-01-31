fd142112db1d2c43539b1f31749a5bd71e1ad370
alchemistbg <alchemist_bg@abv.bg>
2022-01-31 21:28:31 +0100

    Add more options to sequelize.sync() function

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const app = require('./src/app');
 const sequelize = require('./src/config/database');
 
-sequelize.sync({ force: true });
+sequelize.sync({ force: true }).then(async () => {
+	await populateDatabase(25);
+});
 
 app.listen(3000, () => {
 	console.log('Server is up and running on port 3000');
