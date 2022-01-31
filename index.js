0e7ae47b5f90f13628f3773e0d00117eb2b6b9ef
alchemistbg <alchemist_bg@abv.bg>
2022-01-31 21:29:32 +0100

    Add function for automatic populating database wuth users

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const app = require('./src/app');
+const bcrypt = require('bcrypt');
+
+const User = require('./src/user/User');
 const sequelize = require('./src/config/database');
+
+const populateDatabase = async (aUsers, iUsers = 0) => {
+	const hashedPassword = await bcrypt.hash('P4ssword', 10);
+	for (let u = 0; u < aUsers + iUsers; u++) {
+		const user = {
+			username: `user${u + 1}`,
+			email: `user${u + 1}@mail.com`,
+			inactive: u >= aUsers,
+			password: hashedPassword,
+		};
+		await User.create(user);
+	}
+};
 
 sequelize.sync({ force: true }).then(async () => {
 	await populateDatabase(25);
