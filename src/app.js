f430e2e38d35a1a2f95679c52821e434d895ecb9
alchemistbg <alchemist_bg@abv.bg>
2022-02-09 17:32:18 +0100

    Add AuthRouter to the app.js file

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const i18next = require('i18next');
 const backend = require('i18next-fs-backend');
 const middleware = require('i18next-http-middleware');
 
+const AuthRouter = require('./auth/AuthRouter');
 const UserRouter = require('./user/UserRouter');
 const ErrorHandler = require('./error/ErrorHandler');
 
@@ -29,6 +30,7 @@ app.use(middleware.handle(i18next));
 app.use(express.json());
 
 app.use('/api/1.0/users', UserRouter);
+app.use('/api/1.0/auth', AuthRouter);
 
 app.use(ErrorHandler);
 
